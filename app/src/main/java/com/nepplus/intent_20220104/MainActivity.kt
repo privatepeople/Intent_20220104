fcc7311f910fcb5e1da9ff0f133c91b3ab7505f6
Lee Seung Hun <negrat@naver.com>
2022-01-04 10:19:54 +0900

    String을 들고 화면 이동?     - 관련 세팅까지

--- app/src/main/java/com/nepplus/intent_20220104/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220104/MainActivity.kt
@@ -10,6 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSendMessage.setOnClickListener {
+
+//            입력한 내용을 추출
+            val inputMessage = edtMessage.text.toString()
+
+//            메세지 보기 화면으로 이동
+            val myIntent = Intent(this, ViewMessageActivity::class.java)
+            startActivity(myIntent)
+
+
+
+
+        }
+
+
+
+
         btnMoveToOther.setOnClickListener {
 
 //              Intent로, 다른 화면으로 이동해보자.\
--- app/src/main/java/com/nepplus/intent_20220104/OtherActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220104/OtherActivity.kt
@@ -1,11 +1,25 @@
 package com.nepplus.intent_20220104
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//              이전으로 복귀 : 현재화면을 종료.
+            finish()
+
+
+        }
+
     }
 }
